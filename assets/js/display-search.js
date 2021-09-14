47fb01dc0762e4fc0023736bdef703cad92a0a50
BenWofford <woffordbt@gmail.com>
2021-09-13 23:08:46 -0400

    built out additional HTML page & JS script

---
+++ assets/js/display-search.js
@@ -0,0 +1,12 @@
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+// Need to grab query
+/* function getLocation() {
+    var searchLocationResults = document.location.search
+}*/
+
+function searchApi(query) {
+   // var weatherApiUrl = 
+    //TODO: add API
+}
