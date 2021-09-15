5526b127d69f7c2391c13aa24e8ce660e06034e8
BenWofford <woffordbt@gmail.com>
2021-09-14 23:40:56 -0400

    built out README, hit a wall with getting info to show :(

---
+++ README.md
@@ -0,0 +1,42 @@
+# Coding Quiz #
+
+This project's purpose is to check the current and future weather.
+
+You can access this project here: 
+
+## Build Status ##
+
+Unfortunately the div's containing the weather information will not populate.
+
+## Screenshots ##
+
+I've managed to assemble the functions that pull the info from the API's but I can't get the fields containing the data to populate, see below;
+
+![alt=console log of an array of weather data](./images/console-log-of-info.png)
+
+## Features ##
+
+Once functional this site will provide the current and a 5 day forecast.
+
+## Code Example ##
+
+--
+
+## Installation ##
+
+--
+## How To Use ##
+
+--
+
+## Contribute ##
+
+--
+
+## Credits ##
+
+--
+
+## License ##
+
+--
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var searchFormEl = document.querySelector('#search-form');
 var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit() {
 
@@ -41,16 +40,42 @@ searchFormEl.addEventListener('submit', function(event) {
 function searchApi(getLat, getLong) {
 
        var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + getLat + '&lon=' + getLong + '&appid=7a62a0d44214e99149ebe809ba524b67';
-   }
+
 
    fetch(weatherApiUrl)
     .then(function (response) {
         if (!response.ok) {
             throw response.json();
         }
-
         return response.json();
     })
     .then(function (locateResults) {
       console.log(locateResults)
-    });
+        // resultTextEl.textContent = locRes.search.getLat
+    })
+};
+
+function showWeather(resultObj) {
+  console.log(resultObj);
+
+  var currentWeatherCard = document.createElement('div');
+currentWeatherCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var currentWeatherBody = document.createElement('div');
+currentWeatherBody.classList.add('card-body');
+currentWeatherCard.append(currentWeatherBody);
+
+  var titleEl = document.createElement('h2');
+titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('ul');
+bodyContentEl.innerHTML = 
+        '<li>Temp: ' + resultObj.temp + '</li>'
+        '<li>Wind: ' + resultObj.wind + '</li>'
+        '<li>Humidity: ' + resultObj.humidity + '</li>'
+        '<li>UV Index: ' + resultObj.UV + '</li>'  // fix color indication
+
+    resultBody.append(titleEl, currentWeatherBody, currentWeatherCard);
+
+    resultContentEl.append(resultCard);      
+};
