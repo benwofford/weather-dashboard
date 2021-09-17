10bf998a24f7c62b608df1b57c22336ac80b8bb7
BenWofford <woffordbt@gmail.com>
2021-09-17 19:02:17 -0400

    worked with tutor to get cards to display, still not perfect

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,19 +28,24 @@ searchFormEl.addEventListener('submit', function(event) {
   event.preventDefault();
   handleSearchFormSubmit()
    .then(function (response) {
-      var lat = response[0].lat
-      var long = response[0].lon
-      
-      console.log(lat, long);
-
-      searchApi(lat, long);
+      console.log(response);
+      if (!response.length) {
+        console.log('Please try again!'); // TODO: add alert?
+        resultContentEl.innerHTML = '<h3>No results found, please reformat your search.<h3>';
+      } else {
+        var lat = response[0].lat
+        var long = response[0].lon
+        
+        console.log(lat, long);
+  
+        searchApi(lat, long);
+      }
    })
 });
 
 function searchApi(getLat, getLong) {
 
        var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + getLat + '&lon=' + getLong + '&appid=7a62a0d44214e99149ebe809ba524b67';
-
 
    fetch(weatherApiUrl)
     .then(function (response) {
@@ -51,48 +56,86 @@ function searchApi(getLat, getLong) {
     })
     .then(function (locateResults) {
       console.log(locateResults)
+      return locateResults;
     })
     .then(function (locateResults) {
       resultContentEl.textContent = locateResults.searchInputVal;
 
       console.log(locateResults);
 
-      if (!locateResults.results.length) {
-        console.log('Please try again!');
-        resultContentEl.innerHTML = '<h3>No results found, please reformat your search.<h3>';
-      } else {
         resultContentEl.textContent = '';
-        for (var i =0; i < locateResults.results.length; i++) {
-          showWeather(locateResults.results[i]);
+        for (var i =1; i <= 5; i++) {
+          showWeather(locateResults.daily[i]);
         }
-      }
+        showToday(locateResults.daily[0]);
+
     })
     .catch(function (error) {
       console.error(error);
+      console.log('Please try again!');
+      resultContentEl.innerHTML = '<h3>No results found.<h3>';
     });
 };
+
+function showToday(resultObj) {
+  
+  var todayCard = document.createElement('div');
+todayCard.classList.add('card', 'bg-dark', 'text-light', 'today-card');
+
+var todayWeatherBody = document.createElement('div');
+todayWeatherBody.classList.add('card-body');
+
+  var todayTitleEl = document.createElement('h1');
+  var d = new Date(resultObj.dt * 1000);
+todayTitleEl.textContent = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+
+  var iconTodayEl = document.createElement('img');
+
+    iconTodayEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + resultObj.weather[0].icon + '@2x.png')
+
+  var todayBodyContentEl = document.createElement('ul');
+todayBodyContentEl.innerHTML = 
+        '<li>Temp: ' + resultObj.temp.max + '</li>' +
+        '<li>Wind: ' + resultObj.wind_speed + '</li>' +
+        '<li>Humidity: ' + resultObj.humidity + '</li>' +
+        '<li>UV Index: ' + resultObj.uvi + '</li>'  // fix color indication
+
+    todayWeatherBody.append(todayTitleEl, iconTodayEl, todayBodyContentEl);
+    todayCard.append(todayWeatherBody);
+
+    resultContentEl.append(todayCard); 
+
+}
 
 function showWeather(resultObj) {
   console.log(resultObj);
 
   var currentWeatherCard = document.createElement('div');
-currentWeatherCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+currentWeatherCard.classList.add('card', 'bg-dark', 'text-light', 'mb-3', 'p-3', 'five-day-card');
 
   var currentWeatherBody = document.createElement('div');
 currentWeatherBody.classList.add('card-body');
-currentWeatherCard.append(currentWeatherBody);
 
   var titleEl = document.createElement('h2');
-titleEl.textContent = resultObj.title;
+  var d = new Date(resultObj.dt * 1000);
+titleEl.textContent = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+
+  var iconEl = document.createElement('img');
+
+    iconEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + resultObj.weather[0].icon + '@2x.png')
 
   var bodyContentEl = document.createElement('ul');
 bodyContentEl.innerHTML = 
-        '<li>Temp: ' + resultObj.temp + '</li>'
-        '<li>Wind: ' + resultObj.wind + '</li>'
-        '<li>Humidity: ' + resultObj.humidity + '</li>'
-        '<li>UV Index: ' + resultObj.UV + '</li>'  // fix color indication
+        '<li>Temp: ' + resultObj.temp.max + '</li>' +
+        '<li>Wind: ' + resultObj.wind_speed + '</li>' +
+        '<li>Humidity: ' + resultObj.humidity + '</li>' +
+        '<li>UV Index: ' + resultObj.uvi + '</li>'  // fix color indication
 
-    resultBody.append(titleEl, currentWeatherBody, currentWeatherCard);
+    currentWeatherBody.append(titleEl, iconEl, bodyContentEl);
+    currentWeatherCard.append(currentWeatherBody);
 
-    resultContentEl.append(resultCard);      
+    resultContentEl.append(currentWeatherCard);
+    
 };
+
+// currentWeatherCard.prepend(todayCard); -- Y U NO WORK >:(
